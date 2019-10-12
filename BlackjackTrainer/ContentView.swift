2e58aa1112258e8b887e13b79c250d97a4892a3a
Cody Pizzaia <cody@pizzaia.com>
2019-10-12 19:05:55 -0400

    Initial Commit

---
+++ BlackjackTrainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlackjackTrainer
+//
+//  Created by Cody Pizzaia on 10/12/19.
+//  Copyright © 2019 Cody Pizzaia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
